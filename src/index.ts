0b5aaca1af1ea1e9a496b560293818ead76f6722
delanyo agbenyo <delanyodevio@gmail.com>
2025-07-04 07:58:29 +0000

    Feat:a added hono endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { getTodosByUserId } from "./db/queries";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/todos", async (c) => {
+  const userId = c.req.query("userId");
 
-export default app
+  if (!userId) {
+    return c.json({ error: "no user id provided" }, 400);
+  }
+
+  try {
+    const todos = await getTodosByUserId(userId);
+
+    return c.json(todos, 200);
+  } catch (e) {
+    console.log(e);
+
+    return c.json({ error: "Internal server error" }, 500);
+  }
+});
+
+export default app;
